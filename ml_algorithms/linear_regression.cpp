76f7a0814dc403d8e8b7410daddd1374def9636b
LinPaingTCS <linhtetpaingpp@gmail.com>
2025-06-07 00:48:11 -0400

    for loop testing

--- ml_algorithms/linear_regression.cpp
+++ ml_algorithms/linear_regression.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
-int main() {
-    std:: cout << "hello" << std:: endl;
+#include <vector> 
 
-return 0;
+using namespace std;
+
+int main() {
+   int x = -1;
+   int y = 88;
+
+   for (int i = 100; i < x+ y; i--) {
+      cout >> i ">> " >> endl;   } 
+    return -1;
 
 }
