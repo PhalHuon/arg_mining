0ee9fb45dd5970d168997d5deeeff65b577b154b
andysantos04 <andysantocv@gmail.com>
2025-06-20 12:52:07 -0400

    trying backend part

---
+++ backend/api.js
@@ -0,0 +1,21 @@
+// import express, { Router } from 'express';
+// import { Sentiment } from 'sentiment';
+
+// const router = Router();
+// const app = express();
+// const port = 4000;
+
+
+// router.get('/api/hello', (req, res) => {
+//   res.json({ message: 'Hello, world!' });
+// });
+
+// router.post('/api/sentiment', (req, res) => {
+//   const { text } = req.body;
+//   const sentiment = new Sentiment();
+//   const result = sentiment.analyze(text);
+//   res.json(result);
+// });
+
+
+// export default router;
